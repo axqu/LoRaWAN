b5cf1f45b557df837cacc95ccc2bee9667ac148e
axqu <steven@DESKTOP-3609PMO.localdomain>
2021-11-06 20:40:49 -0500

    branch4 intial commit

---
+++ examples/top_python.py
@@ -0,0 +1,15 @@
+
+import sys
+import fig3
+import fig4
+import fig5
+
+
+def main():
+    #fig3.get_fig_3()
+    #fig4.get_fig_4()
+    fig5.get_fig_5()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
